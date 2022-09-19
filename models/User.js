1c6fd60f58d14e0f893dda44083582c9741da686
Hyun Kim <kmasterh@gmail.com>
2022-09-19 22:04:18 +0200

    git init

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = monggose.Schema({
+  name: {
+    type: String,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1
+  },
+  lastname: {
+    type: String,
+    maxlength: 50
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  image: String,
+  token: {
+    type: String
+  },
+  tokenExp: {
+    type: Number
+  }
+})
+
+/* Model은 Schema를 감싸주는 역할 */ 
+const User = mongoose.model('User', userSchema)
